7d2ceb3ad40d9b52c38996bbf81d3344532b86fa
Joachim Løvgaard <joachim@loevgaard.dk>
2024-02-22 12:03:52 +0100

    Add deprecation notice

--- src/DTO/Inventory.php
+++ src/DTO/Inventory.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Setono\Economic\DTO;
 
+/**
+ * @deprecated The inventory module is deprecated in e-conomic and will be removed. Migrate your code to not use this information.
+ */
 final class Inventory
 {
     public float $available = 0;
